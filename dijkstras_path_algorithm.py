18c5c764012dcd996183e7df44813226b57c478d
divyakrishna-devisetty <32887094+divyakrishna-devisetty@users.noreply.github.com>
2017-10-23 12:04:12 -0400

    Dijkstra's algorithm commi #1

---
+++ dijkstras_path_algorithm.py
@@ -0,0 +1,84 @@
+import copy
+import os
+from graph import Graph
+
+import sys
+
+__author__ = 'Divya_krishna_Devisetty'
+
+
+def run_dijkstras(graph, src, dest):
+    visited_nodes = set()
+    distances = {}
+    parent = dict()
+
+    distances[src] = 0
+    minimum_distance_node = src
+    while len(visited_nodes) < len(graph.nodes):
+        # print "current_src_node:", minimum_distance_node
+        if minimum_distance_node == dest:
+            # We build the shortest path and display it
+            path = []
+            pred = dest
+            while pred != None:
+                path.append(pred)
+                pred = parent.get(pred, None)
+            print('shortest path: ' + str(path[::-1]) + " cost=" + str(distances[dest]))
+            return
+
+        for neighbor in graph.edges[minimum_distance_node]:
+            if neighbor not in visited_nodes:
+                # print "neighbor:", neighbor
+                new_distance = distances[minimum_distance_node] + graph.distances[minimum_distance_node][
+                    neighbor]
+                if new_distance < distances.get(neighbor, float('inf')):
+                    distances[neighbor] = new_distance
+                    parent[neighbor] = minimum_distance_node
+                    # print "parent[%s]:" % neighbor, parent[neighbor]
+
+        visited_nodes.add(minimum_distance_node)
+        reference_distances_dict = copy.deepcopy(distances)
+
+        # print "reference_distances_dict:", reference_distances_dict
+        # print "visited_nodes:", visited_nodes
+
+
+        # Delete visited node entries from dictionary so that we calculate minimum distance for the valid ones.
+        for node in visited_nodes:
+            del reference_distances_dict[node]
+        minimum_distance_node = min(reference_distances_dict, key=reference_distances_dict.get)
+
+
+def main():
+    # Read input from file.
+    if not os.path.isfile('dijkstras_input'):
+        print ("Please read input from file: dijkstras_input")
+        sys.exit(1)
+    input_file = open('dijkstras_input', "r")
+    context = file.readlines(input_file)
+    graph = Graph()
+    if context:
+        nodes_input_line = context.pop(0)
+        for node in nodes_input_line.split(','):
+            graph.add_node(node.strip())
+        for lines in context:
+            edge_detail_container = lines.split(',')
+            start_node = edge_detail_container[0]
+            end_node = edge_detail_container[1]
+            distance = int(edge_detail_container[2])
+            graph.add_edge(start_node, end_node, distance)
+            graph.add_node(start_node)
+
+    print("Enter a source node among:",graph.nodes)
+    src = raw_input()
+    print("Enter dest node among:",graph.nodes)
+    dest = raw_input()
+    if src not in graph.nodes:
+        print("Enter a valid source")
+    if dest not in graph.nodes:
+        print("Enter a valid destination")
+
+    run_dijkstras(graph, src, dest)
+
+
+main()
---
+++ graph.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.distances = defaultdict(dict)
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node, distance):
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        # self.distances[(from_node, to_node)] = distance
+        # self.distances[from_node] = {to_node:distance}
+        self.distances[from_node].update({to_node:distance})
+        self.distances[to_node].update({from_node:distance})
